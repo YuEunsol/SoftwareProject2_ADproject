1c8e2f327b013f2a0cd260d57ab102b3e99152d0
U___________Eun_Sol <66423580+YuEunsol@users.noreply.github.com>
2020-11-29 06:49:25 +0900

    search랑 scrap 합쳤음
    해야 할 것 ; 예외처리, 정확도 개선-> 두 줄마다 가사 출력되도록(\n 두개 마다 하나의 리스트 생성, 15줄 이하면 삭제)

--- scrap.py
+++ scrap.py
@@ -9,6 +9,10 @@ class Scrap:
         self.url = url
         self.driver.get(url)
         self.soup = ""
+        self.detail = ""
+        self.title = ""
+        self.comment = []
+
 
 
     def crawlPage(self):
@@ -28,23 +32,59 @@ class Scrap:
 
     def scrapTitleInfo(self):
         titleInfo = self.soup.select_one("title")
-        pprint(titleInfo)
+        # titleInfo = titleInfo
+        # pprint(titleInfo)
         return titleInfo
 
     def scrapDetailInfo(self):
         detailInfo = self.soup.select_one("#description")
-        pprint(detailInfo)
-        return detailInfo
+        detailInfo = str(detailInfo.text)
+        detail = detailInfo.split('\n')[2]
+
+        self.detail = detail
 
     def scrapCommentInfo(self):
         commentInfo = self.soup.select("yt-formatted-string#content-text")
-        pprint(commentInfo)
-        return commentInfo
+        commentInfo = str(commentInfo)
+        commentInfo = commentInfo.split("</span>")
+
+
+        for each in commentInfo:
+            if "dir=\"auto\">" in each:
+                each = each.replace("<span class=\"style-scope yt-formatted-string\" dir=\"auto\">","")
+                each = each.replace("\r","")
+                each = each.replace("\n","")
+                each = each.replace("\xa0","")
+
+                if "<" in each:
+                    each = ""
+                self.comment.append(each)
+
+
+
+
+    def searchSong(self):
+        if '·' in self.detail:
+            return self.detail.split('·')[0]
+        else:
+            return self.title
+
+    def searchArtist(self):
+        return self.detail.split('·')[1].strip()
+
+    def searchLyrics(self):
+        for each in self.comment:
+            if "dir=\"auto\">" not in each:
+                self.comment.remove(each)
+        return self.comment
 
 
 if __name__ == '__main__':
-    crawll = Scrap("https://www.youtube.com/watch?v=kGi4CRlJA6w&list=RDG4o8Qa6pYz4&index=3")
-    crawll.crawlPage()
-    crawll.scrapCommentInfo()
-    crawll.scrapDetailInfo()
-    crawll.scrapTitleInfo()
+    crawl = Scrap("https://www.youtube.com/watch?v=Xp78pd0zkCU&list=RDG4o8Qa6pYz4&index=27")
+    crawl.crawlPage()
+    crawl.scrapCommentInfo()
+    crawl.scrapDetailInfo()
+    crawl.scrapTitleInfo()
+    print(crawl.searchLyrics())
+    print(crawl.searchSong())
+    print(crawl.searchArtist())
