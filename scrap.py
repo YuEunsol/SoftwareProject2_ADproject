c2443ddb20314916387b6f2267f1f10ee69c7f21
U___________Eun_Sol <66423580+YuEunsol@users.noreply.github.com>
2020-11-30 21:01:17 +0900

    잘못된 유튜브 링크 삽입시 에러처리

--- scrap.py
+++ scrap.py
@@ -4,31 +4,37 @@ class Scrap:
     def __init__(self,url):
         self.crawl = Crawl(url)
         self.soup = self.crawl.crawlPage()
+        print(type(self.soup))
 
     def scrapTitleInfo(self):
-        titleInfo = self.soup.select_one("#title").text
-        if titleInfo == "":
-            titleInfo = self.soup.select_one("#container > h1 > yt-formatted-string").text
-        return titleInfo
+        try:
+            titleInfo = self.soup.select_one("#title").text
+            if titleInfo == "":
+                titleInfo = self.soup.select_one("#container > h1 > yt-formatted-string").text
+            return titleInfo
+        except:
+            return "링크를 다시 입력하세요"
 
 
     def scrapDetailInfo(self):
         try:
-            detailInfo = self.soup.select_one("#description")
-            detailInfo = str(detailInfo.text)
+            detailInfo = self.soup.select_one("#description").text
+            return detailInfo
         except:
-            detailInfo = self.soup.select_one("#container > ytd-expander > ytd-metadata-row-container-renderer")
-            detailInfo = str(detailInfo.text)
-        return detailInfo
+            return ""
+
 
     def scrapCommentInfo(self):
-        commentInfo = self.soup.select("yt-formatted-string#content-text")
-        commentInfo = str(commentInfo)
-        commentInfo = commentInfo.split("</yt-formatted-string>")
-        for each in commentInfo:
-            lines = each.split("</span>")
-            if len(lines) > 15:
-                return lines
-        return "댓글 중 가사 댓글이 없습니다"
+        try:
+            commentInfo = self.soup.select("yt-formatted-string#content-text")
+            commentInfo = str(commentInfo)
+            commentInfo = commentInfo.split("</yt-formatted-string>")
+            for each in commentInfo:
+                lines = each.split("</span>")
+                if len(lines) > 15:
+                    return lines
+            return "댓글 중 가사 댓글이 없습니다"
+        except:
+            return ""
 
 
